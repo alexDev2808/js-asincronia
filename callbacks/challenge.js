fb5c670bf189a125a6411c81da76638875addf64
AlexDev2808 <colores.2808.8000@gmail.com>
2023-04-06 19:53:11 -0600

    Fetch Data

--- callbacks/challenge.js
+++ callbacks/challenge.js
@@ -1,4 +1,4 @@
-const XMLHttprequest = require('xmlhttprequest');
+const XMLHttprequest = require('xmlhttprequest').XMLHttpRequest;
 const API = "https://api.escuelajs.co/api/v1";
 
 function fetchData(urlAPI, callback) {
@@ -10,7 +10,7 @@ function fetchData(urlAPI, callback) {
             if(xhttp.status === 200) {
                 callback(null, JSON.parse(xhttp.responseText));
             }else{
-                const error = new Error("Error" + urlAPI);
+                const error = new Error("Error " + urlAPI);
                 return callback(error, null);
             }
         }
@@ -18,3 +18,16 @@ function fetchData(urlAPI, callback) {
 
     xhttp.send();
 }
+
+fetchData(`${API}/products`, function (error1, data1){
+    if (error1) return console.error(error1);
+    fetchData(`${API}/products/${data1[0].id}`, function (error2, data2){
+        if (error2) return console.error(error2);
+        fetchData(`${API}/categories/${data2?.category?.id}`, function(error3, data3) {
+            if (error3) return console.log(error3);
+            console.log(data1[0]);
+            console.log(data2.title);
+            console.log(data3.name);
+        });
+    });
+});
